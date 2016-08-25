c3b5cf0f261d481545b1647fde82c3baa7320803
unknown <>
2016-08-25 17:03:02 +0800

    aa

--- linebottest/views.py
+++ linebottest/views.py
@@ -8,13 +8,14 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 @csrf_exempt
 def callback(request):
-    data = request.POST
-    result = data["result"]
-    for i in result:
-        content = i["content"]
-        text = content["text"]
-        print text
-        
+    print request.body
+    result = request.POST.get('result', False)
+    if result:
+        for i in result:
+            content = i["content"]
+            text = content["text"]
+            print text
+
     return HttpResponse('')
 
 
