33068548c11f7d9ec2a77cfe83cbdbcf392c4f8b
unknown <>
2016-08-25 16:44:12 +0800

    aa

--- linebottest/views.py
+++ linebottest/views.py
@@ -2,9 +2,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
-
+@csrf_exempt
 def callback(request):
     data = request.POST
     print data
