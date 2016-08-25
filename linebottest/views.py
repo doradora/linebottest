c72bce48db35cf2829c77ff34021eb852662c317
unknown <>
2016-08-25 17:10:18 +0800

    aa

--- linebottest/views.py
+++ linebottest/views.py
@@ -3,12 +3,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 
 # Create your views here.
 @csrf_exempt
 def callback(request):
-    result = request.body.get('result', False)
+    data = json.loads(request.body)
+    result = data.get('result',False)
     if result:
         for i in result:
             content = i["content"]
